0e8d2071dd06fce2368993bba48549e889d607b1
Logan McNeely <logan.k.mcneely@gmail.com>
2020-10-27 10:14:45 -0500

    moved get client to private method

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -4,32 +4,28 @@ class Stock < ApplicationRecord
 
   validates :ticker, presence: true
 
-  def self.new_lookup(symbol)
-      new(
-        ticker: symbol,
-      )
-  end
-
   def self.check_db(symbol)
     where(ticker: symbol).first
   end
 
   def get_quote
-    client = IEX::Api::Client.new(
-      publishable_token: Rails.application.credentials.iex_client[:sandbox_tokens][:publishable_token],
-      secret_token: Rails.application.credentials.iex_client[:sandbox_tokens][:secret_token],
-      endpoint: 'https://sandbox.iexapis.com/v1'
-    )
+    client = get_client
     client.quote(self.ticker)
   end
 
   def get_news
-    client = IEX::Api::Client.new(
+    client = get_client
+    client.news(self.ticker)
+  end
+
+  private 
+  
+  def get_client
+    IEX::Api::Client.new(
       publishable_token: Rails.application.credentials.iex_client[:sandbox_tokens][:publishable_token],
       secret_token: Rails.application.credentials.iex_client[:sandbox_tokens][:secret_token],
       endpoint: 'https://sandbox.iexapis.com/v1'
     )
-    client.news(self.ticker)
   end
   
 end
