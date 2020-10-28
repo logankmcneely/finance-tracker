d05a7266adcf240b927b125c0c6df8579b9003d3
Logan McNeely <logan.k.mcneely@gmail.com>
2020-10-28 10:54:23 -0500

    added non-sandbox api call

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -9,23 +9,31 @@ class Stock < ApplicationRecord
   end
 
   def get_quote
-    client = get_client
+    client = get_client(true)
     client.quote(self.ticker)
   end
 
   def get_news
-    client = get_client
+    client = get_client(true)
     client.news(self.ticker)
   end
 
   private 
   
-  def get_client
-    IEX::Api::Client.new(
-      publishable_token: Rails.application.credentials.iex_client[:sandbox_tokens][:publishable_token],
-      secret_token: Rails.application.credentials.iex_client[:sandbox_tokens][:secret_token],
-      endpoint: 'https://sandbox.iexapis.com/v1'
-    )
+  def get_client(sandbox)
+    if sandbox
+      IEX::Api::Client.new(
+        publishable_token: Rails.application.credentials.iex_client[:sandbox_tokens][:publishable_token],
+        secret_token: Rails.application.credentials.iex_client[:sandbox_tokens][:secret_token],
+        endpoint: 'https://sandbox.iexapis.com/v1'
+      )
+    else
+      IEX::Api::Client.new(
+        publishable_token: Rails.application.credentials.iex_client[:core_tokens][:publishable_token],
+        secret_token: Rails.application.credentials.iex_client[:core_tokens][:secret_token],
+        endpoint: 'https://cloud.iexapis.com/v1'
+      )
+    end
   end
   
 end
